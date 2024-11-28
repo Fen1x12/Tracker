2e45361e786d6677402eff6839cdddeb99c8c7fc
Fen1x <baurasm@bk.ru>
2024-11-28 19:55:08 +0300

    Добавил правильно тест и поменял немного подфайл

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -2,7 +2,7 @@
 //  TrackerTests.swift
 //  TrackerTests
 //
-//  Created by  Admin on 27.11.2024.
+//  Created by  Admin on 28.11.2024.
 //
 
 import Testing
@@ -12,14 +12,14 @@ import SnapshotTesting
 @testable import Tracker
 
 final class TrackerTests: XCTestCase {
-
-    @MainActor
-    func testViewController() async {
-        let vc = TrackersViewController()
-        await vc.loadViewIfNeeded()
-
-        assertSnapshot(of: vc, as: .image(traits: .init(userInterfaceStyle: .dark)))
-        assertSnapshot(of: vc, as: .image(traits: .init(userInterfaceStyle: .light)))
-    }
     
+    
+    func testViewController() async {
+        let vc = await TrackersViewController()
+        await vc.loadViewIfNeeded()
+        
+        assertSnapshot(of: vc,as: .image(traits: .init(userInterfaceStyle: .dark)))
+        assertSnapshot(of: vc, as: .image(traits: .init(userInterfaceStyle: .light)))
+        XCTAssertTrue (true)
+    }
 }
